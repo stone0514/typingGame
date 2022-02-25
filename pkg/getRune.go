a06836da552878913d5ee836441ad644c607b116
stone0514 <rkoishi1109@gmail.com>
2022-02-25 23:49:06 +0900

    create getRuneFunc

---
+++ pkg/getRune.go
@@ -0,0 +1,6 @@
+package pkg
+
+func GetRune(s string, i int) rune {
+	rs := []rune(s)
+	return rs[i]
+}
--- test.go
+++ test.go
@@ -1,93 +1,53 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"unicode/utf8"
+
 	"github.com/stone0514/typingGame/internal"
-	"strings"
+	"github.com/stone0514/typingGame/pkg"
 )
 
 func main() {
 
-	// questionList
-	m := map[string]string{"朝": "あさ", "昼": "ひる", "夜": "よる"}
+	/*
+		t := int(75)
+		pkg.Init(t)
 
-	fmt.Println(m)
-
-	// create question & parse
-	for k, v := range m {
-		// japanese
-		fmt.Println(k)
-		// hiragana forAnalysis
-		fmt.Println(v)
-		tgtText := v
-		fmt.Println(tgtText)
-		bigrams, err := ngram(tgtText, 1)
-		if err != nil {
-			fmt.Println(err)
-		}
-		// parse result
-		fmt.Println("bigrams: ", bigrams)
-	}
-
-	// create dictionary
-	dictionary := map[string][]string{
-		"あ": {"a"},
-		"い": {"i", "yi"},
-	}
-	fmt.Println(dictionary)
-	fmt.Println(dictionary["あ"])
-	fmt.Println(dictionary["い"])
-
-	for k, v := range dictionary {
-		fmt.Println(k)
-		fmt.Println(v)
-	}
-
-	// parse sample
-	//tgtText := "本当に今日は良い天気ですね"
-	tgtText := "あいあいあい"
-	bigrams, err := ngram(tgtText, 1)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("bigrams: ", bigrams)
-
-	// correct/wrong judgment
-	for i := 0; len(bigrams) > i; i++ {
-		for k, v := range dictionary {
-			if k == bigrams[i] {
-				fmt.Println("i route: ", v)
-				fmt.Println("k: ", k)
-				fmt.Println("v: ", v)
-			}
-		}
-
-	}
-	fmt.Println("out")
-
-	/* test
-	a := internal.Question()
-	fmt.Println("a:", a)
-
-	for k, v := range a {
-		fmt.Println("k: ", k)
-		fmt.Println("v: ", v)
-	}
+		timeLimit := time.After(time.Duration(t) * time.Second)
 	*/
 
-}
+	q := internal.Question()
+	//d := pkg.Dictionary()
 
-// parse hiragana func
-func ngram(tgtText string, n int) ([]string, error) {
-	spltText := strings.Split(tgtText, "")
-	var ngrams []string
-	if len(spltText) < n {
-		err := errors.New("Error: Input string's length is less than n value")
-		return nil, err
+	//score := 0
+	num := 1
+
+	//ch := pkg.Input(os.Stdin)
+
+	for true {
+		question := q //[score]
+		for k, v := range question {
+			fmt.Printf("[%d] %s\n", num, k)
+			fmt.Printf("[%d] %s\n", num, v)
+
+			i := 0
+			var (
+				uni string
+				bi  string
+			)
+
+			for i < utf8.RuneCountInString(v) {
+				uni = string(pkg.GetRune(v, i))
+				fmt.Println("uni : ", uni)
+				if i+1 < utf8.RuneCountInString(v) {
+					bi = string(pkg.GetRune(v, i))
+					fmt.Println("bi : ", bi)
+				} else {
+					bi = ""
+				}
+				return
+			}
+		}
 	}
-	for i := 0; i < len(spltText)-n+1; i++ {
-		ngrams = append(ngrams, strings.Join(spltText[i:i+n], ""))
-	}
-	return ngrams, nil
 }
